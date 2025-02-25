e4850ede01a4d9f400661e1daf23700ae0f651a3
Tech-Matt <mattia.rizzo.un@gmail.com>
2025-02-25 15:43:57 +0100

    Started Using ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use crossterm::event::{self, Event};
+use ratatui::{text::Text, Frame};
+
 fn main() {
-    println!("Todo App");
+    let mut terminal = ratatui::init();
+    loop {
+        terminal.draw(draw).expect("Failed to draw frame");
+        if matches!(event::read().expect("Failed to read event"), Event::Key(_)) {
+            break;
+        }
+    }
+    ratatui::restore();
+}
+
+fn draw(frame: &mut Frame) {
+    let text = Text::raw("Hello world");
+    frame.render_widget(text, frame.area());
 }
